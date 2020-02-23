5b6ef52fb0c17136b4a102c19e53b97d2945d096
Sebastian Correa <sebas.11992@hotmail.com>
2020-02-23 13:34:01 -0500

    Ejemplo 3 MVC PHP

---
+++ view/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+        <title>Ejemplo 3 MVC PHP</title>
+    </head>
+    <body>
+        <h1>Menú principal</h1>
+        <br>
+        <a href="./?accion=menuCategorias">Categorías productos</a>
+        <br>
+        <a href="./?accion=menuProductos">Productos</a>
+    </body>
+</html>
